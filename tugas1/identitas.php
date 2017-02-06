f761d2a430b3c6d9ddf21948832faa41be7c3b90
sariphidayat94 <sariphidayat94@users.noreply.github.com>
2017-02-06 18:19:56 +0700

    Add files via upload

---
+++ tugas1/identitas.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<body>
+<?php
+
+echo "Selamat Datang di Pembelajaran Web 2";
+$Nama			= "Sarip Hidayat";
+$NIM			= "14.111.227";
+$Kelas			= "TIF K14 Reguler Malam";
+$Mata_Kuliah	= "Pemograman Web 2";
+echo "<br>Nama		: $Nama";
+echo "<br>NIM		: $NIM";
+echo "<br>Kelas		: $Kelas";
+echo "<br>Mata Kuliah : $Mata_Kuliah";
+?>
+</body>
+</head>
+</html> 
